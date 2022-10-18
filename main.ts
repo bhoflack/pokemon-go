88168a05c35ef1a0d74b36ca437b9e39fff3a39c
bhoflack <brecht.hoflack@gmail.com>
2022-10-18 13:23:40 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.forever(function () {
+    radio.sendString("charmander")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
